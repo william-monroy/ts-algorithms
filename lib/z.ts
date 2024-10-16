4f678b9d34d055d2fb7b15a439237e7c54750ed2
María Eugenia Ontiveros Bellés <a01197661@itesm.mx>
2024-10-16 13:02:41 -0600

    feat: 🎸 add z algorithm
    add z algorithm to find a pattern in a string

--- lib/z.ts
+++ lib/z.ts
@@ -1,3 +1,46 @@
-export const z = (s: string):string => {
-  return s + "z";
-};
+function zArray(S: string): number[] {
+  const n = S.length;
+  const Z = new Array(n).fill(0);
+  let l = 0, r = 0, k = 0;
+
+  for (let i = 1; i < n; i++) {
+    if (i > r) {
+      l = r = i;
+      while (r < n && S[r - l] === S[r]) {
+        r++;
+      }
+      Z[i] = r - l;
+      r--;
+    } else {
+      k = i - l;
+      if (Z[k] < r - i + 1) {
+        Z[i] = Z[k];
+      } else {
+        l = i;
+        while (r < n && S[r - l] === S[r]) {
+          r++;
+        }
+        Z[i] = r - l;
+        r--;
+      }
+    }
+  }
+
+  return Z;
+}
+
+function z(T: string, P: string): number[] {
+  const S = P + "$" + T;
+  const l = S.length;
+  const Z = zArray(S);
+  const positions: number[] = [];
+
+  for (let i = 0; i < l; i++) {
+    if (Z[i] === P.length) {
+      positions.push(i - P.length - 1);
+      console.log(i - P.length - 1);
+    }
+  }
+
+  return positions;
+}
